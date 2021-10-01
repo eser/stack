c7334262027e773cf6b4b91986b29707d885f1da
Eser Ozvataf <eser@ozvataf.com>
2021-10-01 17:23:09 +0300

    added more tests for formatters/registry.

--- src/formatters/registry.ts
+++ src/formatters/registry.ts
@@ -1,10 +1,32 @@
 import curryFunctions from "../fp/curry-functions.ts";
 import type Formatter from "./formatter.ts";
 
-function findByName(registry: Registry, name: string): Formatter | undefined {
-  for (const item of registry.items) {
-    if (item.names.includes(name)) {
-      return item;
+//
+// This registry can be used in two ways:
+//
+// Functional way:
+//   import applicationJsonFormatter from "./application-json.ts";
+//   import { findByName } from "./registry.ts";
+//
+//   const formatters = [ applicationJsonFormatter ];
+//   const jsonFormatter = findByName(formatters, "json");
+//
+//
+// Or, object-oriented way:
+//   import applicationJsonFormatter from "./application-json.ts";
+//   import registry from "./registry.ts";
+//
+//   const formatterRegistry = registry([ applicationJsonFormatter ]);
+//   const jsonFormatter = registry.findByName("json");
+//
+
+function findByName(
+  formatters: Iterable<Formatter>,
+  name: string,
+): Formatter | undefined {
+  for (const formatter of formatters) {
+    if (formatter.names.includes(name)) {
+      return formatter;
     }
   }
 
@@ -22,7 +44,7 @@ function registry(formatters: Iterable<Formatter>): Registry {
     items: formatters,
   };
 
-  Object.assign(created, curryFunctions({ findByName }, created));
+  Object.assign(created, curryFunctions({ findByName }, created.items));
 
   return created as Registry;
 }
--- src/formatters/tests/registry.test.ts
+++ src/formatters/tests/registry.test.ts
@@ -1,6 +1,6 @@
 import { asserts } from "./deps.ts";
 import applicationJsonFormatter from "../application-json.ts";
-import registry from "../registry.ts";
+import registry, { findByName } from "../registry.ts";
 
 Deno.test("hex/formatters/registry:basic", () => {
   const formatters = [applicationJsonFormatter];
@@ -9,7 +9,15 @@ Deno.test("hex/formatters/registry:basic", () => {
   asserts.assertEquals([...formatterRegistry.items].length, 1);
 });
 
-Deno.test("hex/formatters/registry:findByName", () => {
+Deno.test("hex/formatters/registry:find-by-name-functional", () => {
+  const formatters = [applicationJsonFormatter];
+
+  const jsonFormatter = findByName(formatters, "json");
+
+  asserts.assertStrictEquals(jsonFormatter, applicationJsonFormatter);
+});
+
+Deno.test("hex/formatters/registry:find-by-name-object-oriented", () => {
   const formatters = [applicationJsonFormatter];
   const formatterRegistry = registry(formatters);
 

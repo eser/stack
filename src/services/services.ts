cf410d81b4b46e58edb5556e52ef8a8b99e47861
Eser Ozvataf <eser@ozvataf.com>
2021-10-02 02:11:47 +0300

    services types in hex/services.

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,29 +1,58 @@
+enum ServiceType {
+  Singleton = "SINGLETON",
+  Factory = "FACTORY",
+}
+
 declare global {
-  var services: Record<string | symbol, unknown>;
+  var services: Record<string | symbol, [ServiceType, unknown]>;
 }
 
 function getService(name: string | symbol, defaultValue?: unknown): unknown {
   if (name in globalThis.services) {
-    return globalThis.services[name];
+    const [serviceType, value] = globalThis.services[name];
+
+    if (serviceType === ServiceType.Factory) {
+      return (value as () => unknown)();
+    }
+
+    return value;
   }
 
   return defaultValue;
 }
 
-function setService(name: string | symbol, value: unknown): void {
+function setValue(name: string | symbol, value: unknown): void {
   globalThis.services = Object.assign({}, globalThis.services, {
-    [name]: value,
+    [name]: [ServiceType.Singleton, value],
+  });
+}
+
+function setFactory(name: string | symbol, value: () => unknown): void {
+  globalThis.services = Object.assign({}, globalThis.services, {
+    [name]: [ServiceType.Factory, value],
   });
 }
 
 function useServices(): [
   (name: string | symbol, defaultValue?: unknown) => unknown,
-  (name: string | symbol, value: unknown) => void,
+  {
+    setValue: (name: string | symbol, value: unknown) => void;
+    setFactory: (name: string | symbol, value: () => unknown) => void;
+  },
 ] {
   return [
     getService,
-    setService,
+    {
+      setValue,
+      setFactory,
+    },
   ];
 }
 
-export { getService, setService, useServices, useServices as default };
+export {
+  getService,
+  setFactory,
+  setValue,
+  useServices,
+  useServices as default,
+};
--- src/services/tests/services.test.ts
+++ src/services/tests/services.test.ts
@@ -1,18 +1,42 @@
 import { asserts } from "./deps.ts";
 import useServices from "../services.ts";
 
-Deno.test("hex/services/services:use-services", () => {
-  const [getService, setService] = useServices();
-  const b = Symbol("b");
+Deno.test("hex/services/services:basic", () => {
+  const [getService, serviceController] = useServices();
 
-  setService("a", 5);
-  setService(b, 6);
-  setService("c", null);
-  setService("d", undefined);
+  serviceController.setValue("a", 5);
 
   asserts.assertEquals(getService("a"), 5);
+});
+
+Deno.test("hex/services/services:with symbols", () => {
+  const [getService, serviceController] = useServices();
+  const b = Symbol("b");
+
+  serviceController.setValue(b, 6);
+
   asserts.assertEquals(getService(b), 6);
+});
+
+Deno.test("hex/services/services:with nullables", () => {
+  const [getService, serviceController] = useServices();
+
+  serviceController.setValue("c", null);
+  serviceController.setValue("d", undefined);
+
   asserts.assertStrictEquals(getService("c"), null);
   asserts.assertStrictEquals(getService("d"), undefined);
   asserts.assertEquals(getService("e", "non-exists"), "non-exists");
+  asserts.assertStrictEquals(getService("f"), undefined);
+});
+
+Deno.test("hex/services/services:with factory", () => {
+  const [getService, serviceController] = useServices();
+
+  let number = 1;
+  serviceController.setFactory("g", () => number++);
+
+  asserts.assertStrictEquals(getService("g"), 1);
+  asserts.assertStrictEquals(getService("g"), 2);
+  asserts.assertStrictEquals(getService("g"), 3);
 });

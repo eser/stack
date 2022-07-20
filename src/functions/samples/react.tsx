93e244ac13779b79dab63d535231d0bc3b467481
Eser Ozvataf <eser@ozvataf.com>
2022-07-20 22:35:38 +0300

    updated typescript sample for functions/react.

--- src/functions/samples/react.tsx
+++ src/functions/samples/react.tsx
@@ -5,6 +5,10 @@ import type {
 } from "../mod.ts";
 import * as hex from "../../mod.ts";
 
+const ParagraphMaker = function ParagraphMaker(props: { text: string }) {
+  return <p>{props.text}</p>;
+};
+
 const main = function main(
   input: HexFunctionInput,
   context: HexFunctionContext,
@@ -12,9 +16,11 @@ const main = function main(
   const to = input.parameters[0] ?? "world";
   const message = `hello ${to}`;
 
-  return hex.functions.results.text(message);
+  return hex.functions.results.reactView(
+    <ParagraphMaker text={message} />,
+  );
 };
 
-hex.functions.dumper(
+hex.functions.dumperReact(
   hex.functions.execute(main),
 );

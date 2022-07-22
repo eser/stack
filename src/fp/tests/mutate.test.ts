bb2c829dede7d5908d82d6ee41fe48c25e7bc215
Eser Ozvataf <eser@ozvataf.com>
2022-07-22 03:36:27 +0300

    added a new test for fp/mutate.

--- src/fp/tests/mutate.test.ts
+++ src/fp/tests/mutate.test.ts
@@ -48,3 +48,29 @@ Deno.test("hex/fp/mutate:array-push", () => {
     },
   );
 });
+
+Deno.test("hex/fp/mutate:with-class", () => {
+  class dummy {
+    items: string[];
+
+    constructor() {
+      this.items = [];
+    }
+  }
+
+  const obj1 = new dummy();
+
+  const result = mutate(obj1, (x) => {
+    x.items.push("laroux");
+  });
+
+  asserts.assertNotStrictEquals(result, obj1);
+  asserts.assertEquals(result.constructor, dummy);
+  asserts.assertEquals(Object.keys(result), ["items"]);
+  asserts.assertEquals(
+    result.items,
+    [
+      "laroux",
+    ],
+  );
+});

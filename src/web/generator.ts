da6166d84d18c75a4b7838f82160144306c073ed
Eser Ozvataf <eser@ozvataf.com>
2022-08-28 12:02:36 +0300

    map generator for experimental package hex/web.

--- src/web/generator.ts
+++ src/web/generator.ts
@@ -25,28 +25,127 @@ const readConfig = async function readConfig(baseDir: string) {
   return makeConfig(config);
 };
 
-const discover = async function discover(baseDir: string, config: Config) {
-  const rootDir = pathPosix.join(baseDir, config.pages?.baseDir ?? ".");
+const checkFileNaming = function checkFileNaming(
+  filename: string,
+  checkEndsWith: boolean,
+  prefix: string,
+  extensions: string[],
+) {
+  for (const extension of extensions) {
+    if (checkEndsWith) {
+      if (filename.endsWith(`${prefix}${extension}`)) {
+        return true;
+      }
+    } else if (filename === `${prefix}${extension}`) {
+      return true;
+    }
+  }
 
-  const map = [];
+  return false;
+};
 
-  for await (const entry of fsWalk.walk(rootDir)) {
-    const shortPath = entry.path.substring(rootDir.length);
+const discoverDirectory = async function discoverDirectory(
+  dir: string,
+  config: Config,
+) {
+  // deno-lint-ignore no-explicit-any
+  const result: any = {
+    subpaths: [],
+    handlers: [], // /index.ts
+    layouts: [], // layout.ts
+    translations: [], // translation.en.ts
+    types: [], // types.ts
+    assets: [], // css, js, svgs, images, fonts, ...
+    others: [], // other files (components etc.)
+  };
 
-    if (shortPath.length === 0) {
+  for await (const entry of fsWalk.walk(dir, { maxDepth: 1 })) {
+    if (entry.path === dir) {
       continue;
     }
 
-    map.push([
-      {
-        path: shortPath,
-        type: entry.isDirectory ? "directory" : "file",
-        virtual: entry.isSymlink,
-      },
-    ]);
+    if (entry.isDirectory) {
+      result.subpaths.push({
+        name: entry.name,
+        items: await discoverDirectory(`${dir}/${entry.name}`, config),
+      });
+
+      continue;
+    }
+
+    // handlers
+    if (
+      checkFileNaming(entry.name, false, "index", config.pages!.extensions!)
+    ) {
+      result.handlers.push({ name: entry.name });
+
+      continue;
+    }
+
+    // layouts
+    if (
+      checkFileNaming(entry.name, false, "layout", config.pages!.extensions!)
+    ) {
+      result.layouts.push({ name: entry.name });
+
+      continue;
+    }
+
+    // translations
+    if (
+      checkFileNaming(
+        entry.name,
+        false,
+        "translations",
+        config.pages!.extensions!,
+      )
+    ) {
+      result.translations.push({ name: entry.name });
+
+      continue;
+    }
+
+    if (
+      checkFileNaming(
+        entry.name,
+        true,
+        ".translations",
+        config.pages!.extensions!,
+      )
+    ) {
+      result.translations.push({ name: entry.name });
+
+      continue;
+    }
+
+    // types
+    if (
+      checkFileNaming(entry.name, false, "types", config.pages!.extensions!)
+    ) {
+      result.types.push({ name: entry.name });
+
+      continue;
+    }
+
+    // assets
+    if (!checkFileNaming(entry.name, false, "", config.pages!.extensions!)) {
+      result.assets.push({ name: entry.name });
+
+      continue;
+    }
+
+    result.others.push({ name: entry.name });
   }
 
-  console.log(map);
+  return result;
+};
+
+const discover = async function discover(baseDir: string, config: Config) {
+  const rootDir = pathPosix.join(baseDir, config.pages?.baseDir ?? ".", "app");
+
+  const map = await discoverDirectory(rootDir, config);
+
+  console.log(JSON.stringify(map, null, 2));
 };
 
 const generate = async function generate(baseDir: string) {

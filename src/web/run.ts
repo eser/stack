b93b8babee9873bbe9437703236904152f1e8c61
Eser Ozvataf <eser@ozvataf.com>
2022-10-24 01:51:35 +0300

    fixed a config issues for hex/web.

--- src/web/run.ts
+++ src/web/run.ts
@@ -1,13 +1,14 @@
-import { type Config } from "./config.ts";
+import { type Config, makeConfig } from "./config.ts";
 import { generator } from "./generator.ts";
 import { urlResolver } from "./url-resolver.ts";
 
 const run = async (config: Config) => {
   const baseDir = Deno.cwd();
 
-  const routes = await generator(baseDir, config);
+  const config_ = makeConfig(config);
+  const routes = await generator(baseDir, config_);
 
-  const resolution = urlResolver("/en/home", routes, config);
+  const resolution = urlResolver("/en/home", routes, config_);
 
   console.log(routes);
   console.log(resolution);

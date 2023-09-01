ca5b2e1a76b1c3fd0002b310afd8bfa162b34564
Eser Ozvataf <eser@ozvataf.com>
2023-09-01 05:02:16 +0300

    feat: [WIP] view adapters (take III).

--- lime/src/dev/imports.ts
+++ lime/src/dev/imports.ts
@@ -3,6 +3,7 @@ export const RECOMMENDED_PREACT_VERSION = "10.17.1";
 export const RECOMMENDED_PREACT_RTS_VERSION = "6.2.1";
 export const RECOMMENDED_PREACT_SIGNALS_VERSION = "1.2.1";
 export const RECOMMENDED_PREACT_SIGNALS_CORE_VERSION = "1.4.0";
+export const RECOMMENDED_PREACT_SIGNALS_REACT_VERSION = "1.3.6";
 export const RECOMMENDED_STD_VERSION = "0.200.0";
 
 export function baseImports(imports: Record<string, string>) {
@@ -11,33 +12,25 @@ export function baseImports(imports: Record<string, string>) {
 }
 
 export function reactImports(imports: Record<string, string>) {
-  imports["react"] =
-    `https://esm.sh/react@${RECOMMENDED_REACT_VERSION}?target=deno`;
-  imports["react/jsx-runtime"] =
-    `https://esm.sh/*react@${RECOMMENDED_REACT_VERSION}/jsx-runtime?target=deno`;
+  imports["react"] = `https://esm.sh/react@${RECOMMENDED_REACT_VERSION}`;
+  imports["react/"] = `https://esm.sh/*react@${RECOMMENDED_REACT_VERSION}/`;
   imports["react-dom"] =
-    `https://esm.sh/react-dom@${RECOMMENDED_REACT_VERSION}?target=deno&external=react`;
-  imports["react-dom/server"] =
-    `https://esm.sh/*react-dom@${RECOMMENDED_REACT_VERSION}/server?target=deno&external=react`;
+    `https://esm.sh/react-dom@${RECOMMENDED_REACT_VERSION}?external=react`;
+  imports["react-dom/"] =
+    `https://esm.sh/*react-dom@${RECOMMENDED_REACT_VERSION}/`;
+  imports["@preact/signals-core"] =
+    `https://esm.sh/@preact/signals-core@${RECOMMENDED_PREACT_SIGNALS_CORE_VERSION}`;
+  imports["@preact/signals-react"] =
+    `https://esm.sh/@preact/signals-react@${RECOMMENDED_PREACT_SIGNALS_REACT_VERSION}?external=react`;
 }
 
 export function preactImports(imports: Record<string, string>) {
-  imports["preact"] =
-    `https://esm.sh/preact@${RECOMMENDED_PREACT_VERSION}?target=deno`;
-  imports["preact/compat"] =
-    `https://esm.sh/*preact@${RECOMMENDED_PREACT_VERSION}/compat?target=deno`;
-  imports["preact/debug"] =
-    `https://esm.sh/*preact@${RECOMMENDED_PREACT_VERSION}/debug?target=deno`;
-  imports["preact/devtools"] =
-    `https://esm.sh/*preact@${RECOMMENDED_PREACT_VERSION}/devtools?target=deno`;
-  imports["preact/jsx-runtime"] =
-    `https://esm.sh/*preact@${RECOMMENDED_PREACT_VERSION}/jsx-runtime?target=deno`;
-  imports["preact/hooks"] =
-    `https://esm.sh/*preact@${RECOMMENDED_PREACT_VERSION}/hooks?target=deno`;
+  imports["preact"] = `https://esm.sh/preact@${RECOMMENDED_PREACT_VERSION}`;
+  imports["preact/"] = `https://esm.sh/*preact@${RECOMMENDED_PREACT_VERSION}/`;
   imports["preact-render-to-string"] =
-    `https://esm.sh/*preact-render-to-string@${RECOMMENDED_PREACT_RTS_VERSION}?target=deno`;
+    `https://esm.sh/*preact-render-to-string@${RECOMMENDED_PREACT_RTS_VERSION}`;
   imports["@preact/signals"] =
-    `https://esm.sh/*@preact/signals@${RECOMMENDED_PREACT_SIGNALS_VERSION}?target=deno`;
+    `https://esm.sh/@preact/signals@${RECOMMENDED_PREACT_SIGNALS_VERSION}?external=preact`;
   imports["@preact/signals-core"] =
-    `https://esm.sh/@preact/signals-core@${RECOMMENDED_PREACT_SIGNALS_CORE_VERSION}?target=deno`;
+    `https://esm.sh/@preact/signals-core@${RECOMMENDED_PREACT_SIGNALS_CORE_VERSION}`;
 }

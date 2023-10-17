7648d07f7c3cbd25384824afbb55c75597a67348
Eser Ozvataf <eser@ozvataf.com>
2023-10-17 22:19:28 +0300

    chore: upgraded fresh fork base to 1.5.2.

--- lime/src/runtime/entrypoints/main_dev.ts
+++ lime/src/runtime/entrypoints/main_dev.ts
@@ -1,3 +1,4 @@
 // FIXME(@eser): temporarily disabled
 // import "preact/debug";
+import "../client.ts";
 export * from "./main.ts";
---
+++ lime/tests/fixture_partials/routes/form_get/index.tsx
@@ -0,0 +1,28 @@
+import { Partial } from "../../../../runtime.ts";
+import { PageProps } from "../../../../server.ts";
+import { Logger } from "../../islands/Logger.tsx";
+
+export default function SlotDemo(props: PageProps) {
+  let value = props.url.searchParams.get("name") ?? "";
+
+  value += value ? "_foo" : "";
+
+  return (
+    <div>
+      <form action="/form_get">
+        <Partial name="slot-1">
+          <p className="status">Default content</p>
+          <p>
+            <input type="text" value={value} name="name" />
+          </p>
+          <Logger name="Form" />
+          <p className="url">{props.url.toString()}</p>
+        </Partial>
+        <button type="submit" className="submit">
+          submit
+        </button>
+      </form>
+      <pre id="logs" />
+    </div>
+  );
+}

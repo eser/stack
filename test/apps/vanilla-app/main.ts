938070b667b70ad0eb6df5e85842cff8e0562550
Eser Ozvataf <eser@acikyazilim.com>
2025-12-27 01:40:51 +0300

    fix: updated validation commands in package.json to use the new validation structure for licenses.

--- test/apps/vanilla-app/main.ts
+++ test/apps/vanilla-app/main.ts
@@ -1,3 +1,4 @@
+// Copyright 2023-present Eser Ozvataf and other contributors. All rights reserved. Apache-2.0 license.
 import * as runtime from "@eser/standards/runtime";
 
 export function main() {
